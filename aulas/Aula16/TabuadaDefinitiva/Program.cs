4d5a60bcfe6b7d8bd827e6b48f6e2c512a1888d0
Guilherme Henrique de Castro <138685301+GuihCastro@users.noreply.github.com>
2025-04-23 17:21:41 -0300

    Adicionados estudos sobre o laço for

---
+++ aulas/Aula16/TabuadaDefinitiva/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TabuadaDefinitiva
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+
+            Console.WriteLine("\tCalculando a tabuada de qualquer número dinamicamente\n");
+
+            Console.Write("Informe o número para saber sua tabuada: ");
+            num = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine("{0} x {1} = {2}", num, i, num * i);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ aulas/Aula17/Media/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Media
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int qtd;
+            double num;
+            double soma = 0;
+            double media;
+
+            Console.WriteLine("\tCalculadora de média aritmética\n");
+
+            Console.Write("Informe de quantos valores será calculada a média aritmética: ");
+            qtd = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < qtd; i++)
+            {
+                Console.Write("Informe o {0}º valor: ", i + 1);
+                num = double.Parse(Console.ReadLine());
+                soma += num;
+            }
+
+            media = soma / qtd;
+
+            Console.WriteLine("\nForam informados {0} números, e a média aritmética deles é: {1}", qtd, media);
+
+            Console.ReadKey();
+        }
+    }
+}
