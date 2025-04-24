2876c90586f051cd1fa7914775cfaeb0e3a3c673
Guilherme Henrique de Castro <138685301+GuihCastro@users.noreply.github.com>
2025-04-23 21:48:49 -0300

    Adicionados estudos sobre o laço while

---
+++ aulas/Aula18-19/NumeroPrimoFor/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumeroPrimoFor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            bool ehPrimo = true;
+
+            Console.WriteLine("\tPrograma que verifica se o númeor informado é primo\n");
+
+            Console.Write("Informe o número que deseja verificar: ");
+            num = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i < num; i**)
+            {
+                if (num % i == 0)
+                {
+                    ehPrimo = false;
+                    break;
+                }
+            }
+
+            if (ehPrimo)
+            {
+                Console.WriteLine("\nO número {0} É primo!", num);
+            }
+            else
+            {
+                Console.WriteLine("\nO número {0} NÃO é primo!", num);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ aulas/Aula18-19/NumeroPrimoWhile/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumeroPrimoWhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            int i;
+            bool ehPrimo = true;
+
+            Console.WriteLine("\tPrograma que verifica se o número informado é primo ou não\n");
+
+            Console.Write("Informe o número que deseja consultar: ");
+            num = int.Parse(Console.ReadLine());
+            i = num - 1;
+
+            while (i > 1)
+            {
+                if (num % i == 0)
+                {
+                    ehPrimo = false;
+                    break;
+                }
+                i--;
+            }
+
+            if (ehPrimo)
+            {
+                Console.WriteLine("\n{0} É um número primo!", num);
+            }
+            else
+            {
+                Console.WriteLine("\n{0} NÃO é um número primo!", num);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
