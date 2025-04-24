e22c7767bf9450c191c7a91835df29e964d9e52b
Guilherme Henrique de Castro <138685301+GuihCastro@users.noreply.github.com>
2025-04-24 09:42:35 -0300

    Adicionados estudos sobre o laço while e o programa do Jogo da Adivinhação

---
+++ aulas/Aula20/VetorDeNomes/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VetorDeNomes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nomes = new string[6];
+
+            Console.WriteLine("\tCriando um vetor com 6 nomes\n");
+
+            for (int i = 0; i < nomes.Length; i++)
+            {
+                Console.Write("Digite o {0}º nome: ", i + 1);
+                nomes[i] = Console.ReadLine();
+            }
+
+            Console.Write("\nNomes: ");
+            for (int i =0; i < nomes.Length; i++)
+            {
+                if (i < nomes.Length - 1)
+                {
+                    Console.Write("{0}, ", nomes[i]);
+                }
+                else
+                {
+                    Console.Write("{0}.", nomes[i]);
+                }
+            }
+
+            Console.ReadKey();
+            Console.Clear();
+        }
+    }
+}
---
+++ aulas/Aula21/JogoDaAdivinhacao/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JogoDaAdivinhacao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] perguntas =
+            {
+                "Tem capa mas não voa...?",
+                "Sem sair do seu canto, é capaz de viajar ao redor do mundo...?",
+                "É alta quando jovem e baixinha quando fica velha. Além disso, é rápida quando magra e lentra quando gorda"
+            };
+            string[] respostas =
+            {
+                "Livro",
+                "Selo",
+                "Vela"
+            };
+            string resposta;
+            string SimOuNao;
+            
+            Console.WriteLine("\tJogo da Adivinhação\n");
+
+            while (true)
+            {
+                int index = new Random().Next(0, perguntas.Length);
+                Console.WriteLine("O que é, o que é: {0}", perguntas[index]);
+                Console.Write("Resposta: ");
+                resposta = Console.ReadLine();
+
+                while (resposta.ToLower() != respostas[index].ToLower())
+                {
+                    Console.Write("Ai que burro! Resposta errada...\nTentar novamente (s/n)? ");
+                    SimOuNao = Console.ReadLine();
+                    if (SimOuNao.ToLower() == "n")
+                    {
+                        break;
+                    }
+                    Console.Write("Resposta: ");
+                    resposta = Console.ReadLine();
+                }
+
+                if (resposta.ToLower() == respostas[index].ToLower())
+                {
+                    Console.WriteLine("\nResposta correta!");
+                }
+                else
+                {
+                    Console.WriteLine("\nA resposta certa era: {0}", respostas[index]);
+                }
+
+                Console.Write("\nQuer jogar novamente (s/n)? ");
+                SimOuNao = Console.ReadLine();
+
+                if (SimOuNao.ToLower() == "n")
+                {
+                    Console.WriteLine("\nObrigado por jogar!");
+                    break;
+                }
+
+                Console.Clear();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
